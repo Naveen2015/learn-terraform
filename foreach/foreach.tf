1f704ef32ff30ddef461e757d952884ee7f8ab5c
naveenporeddy <naveenkumar9707@gmail.com>
2025-04-17 17:12:48 -0500

    foreach

---
+++ foreach/foreach.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.3"
+    }
+  }
+}
+variable "fruits" {
+  default = {
+    apple = {
+      name = "apple"
+      qty = 10
+    }
+    mango = {
+      name = "mangod"
+      qty = 20
+    }
+  }
+}
+
+resource "null_resource" "fruits" {
+
+  for_each = var.fruits
+
+  provisioner "local-exec" {
+    command = "echo fruit name: ${each.key} :::: ${each.value["name"]}:::: ${each.value["qty"]}"
+  }
+}
